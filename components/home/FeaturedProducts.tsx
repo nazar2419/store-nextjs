f9de577557c81aa288d013e5f30ae8f097122d75
Nazar <nazarx095@gmail.com>
2026-01-29 15:38:01 +0200

    product grid component

---
+++ components/home/FeaturedProducts.tsx
@@ -0,0 +1,17 @@
+import { fetchFeatureProducts } from "@/utils/actions"
+import EmptyList from "../global/EmptyList";
+import SectionTitle from "../global/SectionTitle";
+import ProductsGrid from "../products/ProductsGrid";
+
+
+async function FeaturedProducts() {
+  const products = await fetchFeatureProducts();
+  if (products.length === 0)   return <EmptyList />;
+  return (
+    <section className="pt-24">
+      <SectionTitle text="featured products" />
+      <ProductsGrid products={products}/>
+    </section>
+  )
+}
+export default FeaturedProducts
